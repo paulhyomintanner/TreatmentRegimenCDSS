f60eb66a434d3154eb38a07c58edae48129188e0
paulhyomintanner <paul.tanner@students.fhnw.ch>
2024-02-12 17:57:58 +0100

    returned the sorted list instead of a new one

--- 2nd iteration/rank_treatments.py
+++ 2nd iteration/rank_treatments.py
@@ -1,2 +1,4 @@
 def rank_treatments(treatments, preferred_cpg):
-    return sorted(treatments, key=lambda x: x['rank'][0][preferred_cpg])
+    treatments.sort(key=lambda x: x['rank'][0][preferred_cpg])
+    return treatments
+#should just sort the list instead of sorting and returning a new list. 
