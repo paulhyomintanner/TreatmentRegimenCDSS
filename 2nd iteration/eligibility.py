f455a7cd5183a5be8cc73bb05c41d6835f179ea1
paulhyomintanner <paul.tanner@students.fhnw.ch>
2024-02-12 22:35:32 +0100

    made the exclusion case sensitive again

--- 2nd iteration/eligibility.py
+++ 2nd iteration/eligibility.py
@@ -1,15 +1,11 @@
 def filter_by_severity(treatments, severity): 
     treatments[:] = [treatment for treatment in treatments if treatment['severity'].lower() == severity.lower()]
     return treatments
-# Filters the treatments based on the user inputted severity i.e. low or high
 
 def filter_by_exclusions(treatments, exclusions):
-    exclusions = [exclusion.strip().lower() for exclusion in exclusions if exclusion.strip()]  
+    exclusions = [exclusion.strip().lower() for exclusion in exclusions if exclusion.strip()] 
     treatments[:] = [treatment for treatment in treatments 
                      if not any(exclusion in [excl.lower() for excl in medication.get('exclusion', [])]  
                                 for medication in treatment['medication'] 
                                 for exclusion in exclusions)]
     return treatments
-
-  # Filters the treatments based on the user inputted exclusion i.e. conditionX or pregnancy etc.
-
