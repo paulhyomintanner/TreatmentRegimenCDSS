68f002b49e4bac1888915094f60c31a762d3ffb5
paulhyomintanner <paul.tanner@students.fhnw.ch>
2024-02-27 22:30:16 +0100

    gui redering, scroll bar, and printing issues

--- db/drop_down.py
+++ db/drop_down.py
@@ -1,6 +1,9 @@
 import customtkinter as ctk
 import json
 from tkinter import messagebox
+import tkinter as tk
+
+
 
 class TreatmentApp:
     def __init__(self, root):
@@ -116,22 +119,38 @@ class TreatmentApp:
         self.page_two = ctk.CTkFrame(self.root)
         self.page_two.pack(fill="both", expand=True)
 
+        self.canvas = tk.Canvas(self.page_two)  
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.configure(bg='#2D2D2D') 
+
+        scrollbar = tk.Scrollbar(self.page_two, bg='#2D2D2D', troughcolor='#4D4D4D', activebackground='#4D4D4D', command=self.canvas.yview)
+        scrollbar.pack(side="right", fill="y")
+
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+
+        self.inner_frame = tk.Frame(self.canvas, bg='#2D2D2D')  
+        self.canvas.create_window((0,   0), window=self.inner_frame, anchor="nw")
+
         diseases = list(set(treatment['disease'] for treatment in self.data['_default'].values()))
         severity = list(set(eligibility['severity'] for treatment in self.data['_default'].values() for eligibility in treatment['eligibility']))
 
-        ctk.CTkLabel(self.page_two, text="Select a disease:").pack(pady=5)
-        self.disease_dropdown = ctk.CTkOptionMenu(self.page_two, values=diseases)
+        ctk.CTkLabel(self.inner_frame, text="Select a disease:").pack(pady=5)
+        self.disease_dropdown = ctk.CTkOptionMenu(self.inner_frame, values=diseases)
         self.disease_dropdown.pack(pady=5)
 
-
-        ctk.CTkLabel(self.page_two, text="Select a severity:").pack(pady=5)
-        self.severity_dropdown = ctk.CTkOptionMenu(self.page_two, values=severity)
+        ctk.CTkLabel(self.inner_frame, text="Select a severity:").pack(pady=5)
+        self.severity_dropdown = ctk.CTkOptionMenu(self.inner_frame, values=severity)
         self.severity_dropdown.pack(pady=5)
 
-        ctk.CTkLabel(self.page_two, text="Click submit for each disease").pack(pady=5)
-        ctk.CTkButton(self.page_two, text="Submit", command=self.submit_page_two).pack(pady=10)
-        ctk.CTkButton(self.page_two, text="Retrieve Treatments", command=self.retrieve_treatments).pack(pady=10)
-        ctk.CTkButton(self.page_two, text="Return to Start", command=self.return_to_page_one).pack(pady=10)
+        ctk.CTkLabel(self.inner_frame, text="Click submit for each disease").pack(pady=5)
+        ctk.CTkButton(self.inner_frame, text="Submit", command=self.submit_page_two).pack(pady=10)
+        ctk.CTkButton(self.inner_frame, text="Retrieve Treatments", command=self.retrieve_treatments).pack(pady=10)
+        ctk.CTkButton(self.inner_frame, text="Return to Start", command=self.return_to_page_one).pack(pady=10)
+
+        self.canvas.update_idletasks() 
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+
 
     def load_superseding_rules(self):
         with open('superseding_rules_db.json') as f:
@@ -157,29 +176,28 @@ class TreatmentApp:
                     candidate_treatments[disease_info["disease"]] = ranked_treatments[0]
                 else:
                     no_treatment_text = f"No treatments found for {disease_info['disease']}"
-                    ctk.CTkLabel(self.page_two, text=no_treatment_text).pack(pady=10)
-           
+                    ctk.CTkLabel(self.inner_frame, text=no_treatment_text).pack(pady=10)
+            
             recommended_treatments = {}
             
             if candidate_treatments:
                 recommended_treatments = self.rules(candidate_treatments)
 
-                for disease, treatment in recommended_treatments.items():
-                    medication_details = ', '.join([f"{key}: {value}" for medication in treatment['medication'] for key, value in medication.items() if key in ["drug", "form", "site", "route", "method"]])
-                    treatment_text = f"Recommended treatment for {disease}:\nTreatment ID: {treatment['treatment_id']}\nDescription: {treatment['description']}\nMedication details: {medication_details}"
-                    ctk.CTkLabel(self.page_two, text=treatment_text).pack(pady=10)
-            else:
-                ctk.CTkLabel(self.page_two, text="No recommended treatments after applying rules.").pack(pady=10)
-            
-            self.recommended_treatments = recommended_treatments  
-            print(self.recommended_treatments)  
+            for disease, treatment in recommended_treatments.items():
+                medication_details = ', '.join([f"{key}: {value}" for medication in treatment['medication'] for key, value in medication.items() if key in ["drug", "form", "site", "route", "method"]])
+                treatment_text = f"Recommended treatment for {disease}:\nTreatment ID: {treatment['treatment_id']}\nDescription: {treatment['description']}\nMedication details: {medication_details}"
+                ctk.CTkLabel(self.inner_frame, text=treatment_text).pack(pady=10)
 
+            if not recommended_treatments:
+                ctk.CTkLabel(self.inner_frame, text="No recommended treatments after applying rules.").pack(pady=10)
 
+            ctk.CTkButton(self.inner_frame, text="Confirm Treatments", command=self.confirm_treatments).pack(pady=10)
+            ctk.CTkButton(self.inner_frame, text="Reject Treatment", command=self.reject_treatment_ui).pack(pady=10)
 
-            self.recommended_treatments = recommended_treatments  
-            ctk.CTkButton(self.page_two, text="Confirm Treatments", command=self.confirm_treatments).pack(pady=10)
-            ctk.CTkButton(self.page_two, text="Reject Treatment", command=self.reject_treatment_ui).pack(pady=10)
+            self.root.update_idletasks()  
+            self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
+            self.recommended_treatments = recommended_treatments   
             if self.recommended_treatments:
                 break
 
@@ -222,6 +240,9 @@ class TreatmentApp:
 
     def confirm_treatments(self):
         for disease, treatment in self.recommended_treatments.items():
+            treatment_text = f"Recommended treatment for {disease}:\nTreatment ID: {treatment['treatment_id']}\nDescription: {treatment['description']}"
+            ctk.CTkLabel(self.inner_frame, text=treatment_text).pack(pady=10)
+
             for medication in treatment['medication']:
                 medication_details = ', '.join([f"{key}: {value}" for key, value in medication.items() if key in ["drug", "form", "site", "route", "method"]])
                 dose_strategy = medication['dose_strategy']
@@ -233,23 +254,16 @@ class TreatmentApp:
                 period = dose_strategy['maxDosePerPeriod']['denominator']['value']
                 period_unit = dose_strategy['maxDosePerPeriod']['denominator']['unit']
 
-                treatment_text = f"Recommended treatment for {disease}:\nTreatment ID: {treatment['treatment_id']}\nDescription: {treatment['description']}\nMedication details: {medication_details}\nInstruction: {instruction}\nAdditional instruction: {additional_instruction}\nPatient instruction: {patient_instruction}\nMax dose: {max_dose} {max_dose_unit} per {period} {period_unit}"
-                print(treatment_text)
-                ctk.CTkLabel(self.page_two, text=treatment_text).pack(pady=10)
+                medication_text = f"Medication details: {medication_details}\nInstruction: {instruction}\nAdditional instruction: {additional_instruction}\nPatient instruction: {patient_instruction}\nMax dose: {max_dose} {max_dose_unit} per {period} {period_unit}"
+                ctk.CTkLabel(self.inner_frame, text=medication_text).pack(pady=10)
+
+        self.root.update_idletasks()
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
     def reject_treatment_ui(self):
-        self.reject_entry = ctk.CTkEntry(self.page_two, placeholder_text="Type ID or IDs to reject")
+        self.reject_entry = ctk.CTkEntry(self.inner_frame, placeholder_text="Type ID or IDs to reject")
         self.reject_entry.pack(pady=5)
-        ctk.CTkButton(self.page_two, text="Confirm Rejection", command=self.handle_rejection).pack(pady=10)
-
-
-
-
-
-
-
-
-
+        ctk.CTkButton(self.inner_frame, text="Confirm Rejection", command=self.handle_rejection).pack(pady=10)
 
 def main():
     root = ctk.CTk()
