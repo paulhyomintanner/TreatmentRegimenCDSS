3acc95819076a07a29c1d14ef997cc6547dc810d
paulhyomintanner <paul.tanner@students.fhnw.ch>
2024-04-12 22:08:12 +0200

    Added: dosage ui, strat choice, concentration

--- Flet/app.py
+++ Flet/app.py
@@ -43,6 +43,8 @@ def main(page: Page) -> None:
     selected_cpg_data = {}  
     candidate_treatments = {}
     excluded_treatments = []
+    result = []
+
 
 
     def route_change(e: RouteChangeEvent) -> None:
@@ -258,31 +260,40 @@ def main(page: Page) -> None:
 
 
         def check_superseding_rules(highest_ranked_treatments, rules):
-            treatments_dict = {t['treatment_id']: t for t in highest_ranked_treatments}
+            treatment_ids = {treatment['treatment_id']: treatment for treatment in highest_ranked_treatments}
+            superseded_treatments = []
+
+            #Process each rule and adjust treatments list accordingly
             for rule_details in rules['_default'].values():
                 pair = rule_details['pair']
                 superseding_id = rule_details['superseding_id']
 
-                #Check if both treatments in the pair are present in the highest ranked treatments list
-                if all(treatment_id in treatments_dict for treatment_id in pair):
-                    #If a pair is found, determine which one is superseded
-                    for treatment_id in pair:
-                        if treatment_id != superseding_id:
-                            diseaseA = treatments_dict[treatment_id]['disease']
-                            diseaseB = treatments_dict[superseding_id]['disease']
-                            superseding_rule = f"Superseding Rule Triggered: {rule_details}"
-                            message = f"Treatment {superseding_id} can be used for both {diseaseA} and {diseaseB}."
-                            return (treatments_dict[superseding_id], superseding_rule, message)
-            return (None, "No superseding rules apply.", None)
+                #Check if all members of a rule pair are present in the highest ranked treatments
+                if all(treatment_id in treatment_ids for treatment_id in pair):
+                    superseded = [tid for tid in pair if tid != superseding_id]
 
-        
+                    superseded_treatments.extend(superseded)
+
+                    #Make messages for the superseding treatment
+                    disease_names = [treatment_ids[tid]['disease'] for tid in pair]
+                    superseding_rule = f"Superseding Rule Triggered: {rule_details}"
+                    message = f"Treatment {superseding_id} can be used for both {', '.join(disease_names)}."
+                    return (superseding_id, superseding_rule, message, [treatment_ids[superseding_id]])
+
+            #Filter out any superseded treatments from the list if no rules applied
+            if superseded_treatments:
+                highest_ranked_treatments = [t for t in highest_ranked_treatments if t['treatment_id'] not in superseded_treatments]
+
+            return (None, "No superseding rules apply.", None, highest_ranked_treatments)
+
         def retrieve_treatments(user_data, selected_cpg_data):
             user_diseases = user_data['diseases']
             user_dob = user_data['dob']
             age_in_days, age_in_years = calculate_age_in_days_and_years(user_dob)
             user_weight = int(user_data['weight'])
             user_exclusions = user_data['exclusions']
-
+            
+            global highest_ranked_treatments
             highest_ranked_treatments = []
             output_text = []
 
@@ -294,7 +305,7 @@ def main(page: Page) -> None:
                 for treatment in selected_cpg_data['_default'].values():
                     if treatment['disease'] == disease_name:
                         for eligibility in treatment['eligibility']:
-                            severity_condition = f"{list(eligibility['severity'].keys())[0]}: {list(eligibility['severity'].values())[0]}"
+                            severity_condition = "{}: {}".format(list(eligibility['severity'].keys())[0], list(eligibility['severity'].values())[0])
                             if disease_severity == severity_condition:
                                 patient_profile = eligibility['patient_profile']
                                 age_range_unit = patient_profile.get('age_range_unit', 'years')
@@ -304,28 +315,26 @@ def main(page: Page) -> None:
 
                                 user_age = age_in_days if age_range_unit == 'days' else age_in_years
 
-                                if (age_range['min'] <= user_age <= age_range['max'] and 
-                                    user_weight >= min_weight and 
+                                if (age_range['min'] <= user_age <= age_range['max'] and
+                                    user_weight >= min_weight and
                                     not any(exclusion in user_exclusions for exclusion in exclusions) and
-                                    treatment['treatment_id'] not in excluded_treatments):  # Check if treatment is not excluded
+                                    treatment['treatment_id'] not in excluded_treatments):  #Check if treatment is not excluded
                                     eligible_treatments.append(treatment)
-
 
                 if eligible_treatments:
                     eligible_treatments.sort(key=lambda x: list(x['rank'][0].values())[0])
                     highest_ranked_treatments.append(eligible_treatments[0])
-
                 else:
                     print(f"No treatment found for {disease_name} with severity {disease_severity}")
                     output_text.append(f"No treatment found for {disease_name} with severity {disease_severity}")
 
+            superseding_id, superseding_rule, message, updated_highest_ranked_treatments = check_superseding_rules(highest_ranked_treatments, rules)
+            highest_ranked_treatments = updated_highest_ranked_treatments  #Update the list based on superseding rules
+            print(highest_ranked_treatments)
 
-            result = check_superseding_rules(highest_ranked_treatments, rules)
-            
-
-            if result[0]:  
-                treatment_details = f"{result[1]}\n{result[2]}\nDetailed Information for {result[0]['treatment_id']}:\nDescription: {result[0]['description']}"
-                for medication in result[0]['medication']:
+            if superseding_id:  
+                treatment_details = f"{superseding_rule}\n{message}\nDetailed Information for {superseding_id}:\nDescription: {highest_ranked_treatments[0]['description']}"
+                for medication in highest_ranked_treatments[0]['medication']:
                     treatment_details += f"\n  Medication: {medication['drug']}, Form: {medication['form']['type']}, Dose: {medication['dose_strategy'][0]['instruction']}"
                 output_text.append(treatment_details)
             else:
@@ -334,25 +343,26 @@ def main(page: Page) -> None:
                     for medication in treatment['medication']:
                         treatment_details += f"\n  Medication: {medication['drug']}, Form: {medication['form']['type']}, Dose: {medication['dose_strategy'][0]['instruction']}"
                     output_text.append(treatment_details)
-                output_text.append(result[1])
+                output_text.append(superseding_rule)  #This should be the "No superseding rules apply." message
 
             recommended_treatments_text.value = "\n\n".join(output_text)
             print(recommended_treatments_text.value)
-            #Updating the radio buttons dynamically to reflect current options based on the highest ranked treatments.
+
+            #Update the radio buttons dynamically update the current options based on the highest ranked treatments
             rejection_radiobuttons.content = ft.Column([
                 ft.Radio(value=t_id, label=t_id) for t_id in [t['treatment_id'] for t in highest_ranked_treatments]
             ])
             page.update()
-            return result, [t['treatment_id'] for t in highest_ranked_treatments], highest_ranked_treatments
-
+            return superseding_id, [t['treatment_id'] for t in highest_ranked_treatments], highest_ranked_treatments
 
 
         recommended_treatments_text = Text(value='')
 
         rejection_radiobuttons = ft.RadioGroup(content=ft.Column([]))
-        reject_button = ElevatedButton(text='Submit', on_click=lambda _: get_rejection(rejection_radiobuttons.value))
+        reject_button = ElevatedButton(text='Click to reject', on_click=lambda _: get_rejection(rejection_radiobuttons.value))
         rejection_text = ft.Text()
 
+        
         def get_rejection(e):
             excluded_treatments.append(rejection_radiobuttons.value)
             retrieve_treatments(user_data, selected_cpg_data) 
@@ -377,7 +387,7 @@ def main(page: Page) -> None:
                             reject_button,
                             rejection_text,
                         ],
-                        height=500,
+                        height=600,
                         width=350,
                         scroll=ft.ScrollMode.ALWAYS
                     ),
@@ -388,20 +398,86 @@ def main(page: Page) -> None:
             )
         )
             
+        
+        
+        
+        dose_content = ft.Column(
+            controls=[],
+            height=350,
+            width=350,
+            spacing=5,
+            scroll=ft.ScrollMode.ALWAYS
+        )
+
+        dose_container = ft.Container(
+            content=dose_content,
+            width=350,
+            height=350,
+            bgcolor=ft.colors.INDIGO_100,
+            border=ft.border.all(1, ft.colors.BLACK),
+            border_radius=ft.border_radius.all(5),
+            padding=ft.padding.all(10)
+        )
+
+        def retrieve_dosing_guide(e):
+            dose_content.controls.clear()  
+            for treatment in highest_ranked_treatments:
+                dose_content.controls.append(Text(value=f"Treatment ID: {treatment['treatment_id']} - {treatment['disease']}"))
+                for medication in treatment['medication']:
+                    dose_content.controls.append(Text(value=f"Medication: {medication['drug']}"))
+                    for idx, strategy in enumerate(medication['dose_strategy']):
+                        checkbox = ft.Checkbox(
+                            label=f"{strategy['strategy']} - {strategy['calculation']} - {strategy['therapeuticDose']}",
+                            data=strategy #store strat. for later use 
+                        )
+                        dose_content.controls.append(checkbox)
+                        
+                        # Check if the medication is divisible and add a TextField if it is so the user can input a concentration
+                        if medication['form']['divisible']:
+                            concentration_input = TextField(
+                                label="Concentration mg/unit",
+                                hint_text="Enter concentration",
+                                width=300  
+                            )
+                            dose_content.controls.append(concentration_input)
+
+            submit_button = ElevatedButton(text="Submit Choices", on_click=submit_choices)
+            dose_content.controls.append(submit_button)  
+            page.update()
+
+        def submit_choices(e):
+            selected_strategies = []
+            for control in dose_content.controls:
+                if isinstance(control, ft.Checkbox) and control.value:
+                    selected_strategies.append(control.data)  #Retrieve strategies data from the checkbox
+            print("Selected Strategies:", selected_strategies)
+            page.update()
+
         if page.route == '/SelectDosingStrategy':
             page.views.append(
-            View(
-                route='/SelectDosingStrategy',
-                controls=[
-                    Text(value='User input', size=30),
-                    ElevatedButton(text='Go back', on_click=lambda _: page.go('/SelectTreatmentPlan')),
-                    ElevatedButton(text='Build Regimen', on_click=lambda _: page.go('/BuildRegimen'))
-                ],
-                vertical_alignment=MainAxisAlignment.CENTER,
-                horizontal_alignment=CrossAxisAlignment.CENTER,
-                spacing=20
+                View(
+                    route='/SelectDosingStrategy',
+                    controls=[
+                        Column(
+                            controls=[
+                                Text(value='Dosing Strategy', size=30),
+                                ElevatedButton(text='Go back', on_click=lambda _: page.go('/SelectTreatmentPlan')),
+                                ElevatedButton(text='Get Dosing strategies', on_click=retrieve_dosing_guide),
+                                dose_container,
+                                ElevatedButton(text='Build Regimen', on_click=lambda _: page.go('/BuildRegimen')),
+                            ],
+                            height=700,
+                            width=350,
+                            scroll=ft.ScrollMode.ALWAYS
+                        ),
+                    ],
+                    vertical_alignment=MainAxisAlignment.CENTER,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                    spacing=20
+                )
             )
-        )
+
+    
             
         if page.route == '/BuildRegimen':
             page.views.append(
